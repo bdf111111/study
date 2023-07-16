8c2f93b388609f91ee85e46335f9db6be5caff80
xiaoxin <1@163.com>
2023-07-16 23:05:52 +0800

    rocketmq

---
+++ rocketmq/src/main/java/com/hgf/study/rocketmq/RocketmqApplication.java
@@ -0,0 +1,15 @@
+package com.hgf.study.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqApplication.class, args);
+    }
+
+}
