cd35ebdeb5b680a2a58f698d388bcc670c6f68fe
xiaoxin <1@163.com>
2023-08-21 21:57:39 +0800

    code->IOTest

--- code/src/main/java/com/hgf/study/code/IOTest.java
+++ code/src/main/java/com/hgf/study/code/IOTest.java
@@ -1,6 +1,8 @@
 package com.hgf.study.code;
 
 import java.io.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,6 +14,7 @@ import java.util.Scanner;
  */
 public class IOTest {
     public static void main(String[] args) {
+        in = new int[0];
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         File a = new File("a.txt");
